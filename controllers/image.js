860fb10838c2cb165b62db753accb8fa6a68740c
Chris <cthauerdev@gmail.com>
2018-10-14 15:28:43 -0400

    changed image.js

--- controllers/image.js
+++ controllers/image.js
@@ -21,4 +21,7 @@ const handleImage = (req, res, db) => {
     .catch(err => res.status(400).json('unable to get entries'));
 }
 
-module.exports = { handleImage, handleApiCall }
+module.exports = {
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
+}
