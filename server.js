de4d8b67d547701e8b865b097ea360ae9a2edbe0
Chris <cthauerdev@gmail.com>
2018-10-14 15:12:39 -0400

    changed db connection info

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-regular-71265',
-    user : 'postgres',
-    password : 'Starwarsq1',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
